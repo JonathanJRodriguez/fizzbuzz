f9c6a123b92f2d7c5d913fea5a095a712ab5f5f5
Jonathan Rodriguez <launchx06724@innovaccion.mx>
2022-04-26 19:44:38 -0500

    add: Test for Reader class

---
+++ test/models/Reader.test.js
@@ -0,0 +1,9 @@
+const Reader = require("./../../lib/utils/Reader")
+
+describe("Unit tests for Reader class", () => {
+    test("Create a Reader object", () => {
+        const explorers = Reader.readJsonFile("explorers.json");
+
+        expect(explorers.length).toBe(15)
+    })
+})
